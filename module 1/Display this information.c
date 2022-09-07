bd2080835f1cdad54f406ef9db49ac5ea20dd5e2
Chabhadiya dhrupal <110007881+chabhadiyadhrupal@users.noreply.github.com>
2022-09-07 10:04:17 +0530

    Add files via upload

---
+++ module 1/Display this information.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("my name is dhrupal");
+	printf("\nmy brith date is:20/7/2005");
+	printf("\nmy age is :17");
+	printf("\nmy address is:latipur ta :dhrol dist : jamnagar");
+}
+
