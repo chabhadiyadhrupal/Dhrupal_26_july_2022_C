4e3183733752d8866fe15b57e3eee067b965d717
Chabhadiya dhrupal <110007881+chabhadiyadhrupal@users.noreply.github.com>
2022-09-08 08:25:21 +0530

    Create 6. Reverse Order.c

---
+++ module 3/6. Reverse Order.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int a, b, c, d;
+    printf("\nEnter number you want to print in reverse order:- ");
+    scanf("%d",&a);
+	for(d=0; d<10; d++)
+	{
+		b = a % 10;
+		c = (c * 10) + b;
+		a = a / 10;
+		if (a == 0)
+		{
+			d = 10;
+		}
+		if (d == 10)
+		{
+			printf("\nReverse order of number is :- %d",c);
+		}
+	}
+}
