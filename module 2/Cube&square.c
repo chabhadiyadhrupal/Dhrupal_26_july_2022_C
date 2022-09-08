db8f4dd3ffdf9a6644f6578e904320005cc62cd6
Chabhadiya dhrupal <110007881+chabhadiyadhrupal@users.noreply.github.com>
2022-09-08 08:15:27 +0530

    Create Cube&square.c

---
+++ module 2/Cube&square.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main ()
+{
+	long int a,b,c;
+	printf("enter your digit to find square and cube:");
+	scanf("%d",&a);
+	b=a*a;
+	printf("\nyour no of square is :%Id",b);
+	c=a*a*a;
+	printf("\nyour no of cube is :%Id",c);	
+}
